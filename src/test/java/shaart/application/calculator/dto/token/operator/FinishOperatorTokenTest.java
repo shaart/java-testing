74db8d7aa365e826084d66887f9d148ae8975873
nikita.gusakov <nikit.gusakoff@gmail.com>
2020-09-14 14:46:55 +0300

    Добавил метод инициализации и поправил замечания.

--- src/test/java/shaart/application/calculator/dto/token/operator/FinishOperatorTokenTest.java
+++ src/test/java/shaart/application/calculator/dto/token/operator/FinishOperatorTokenTest.java
@@ -1,22 +1,32 @@
 package shaart.application.calculator.dto.token.operator;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import shaart.application.calculator.dto.token.value.ValueToken;
 
 @ExtendWith(MockitoExtension.class)
 public class FinishOperatorTokenTest {
 
-    FinishOperatorToken finishOperatorToken = new FinishOperatorToken();
+    private FinishOperatorToken finishOperatorToken;
+
+    @BeforeEach
+    void initialization() {
+        finishOperatorToken = new FinishOperatorToken();
+    }
 
     @Test
     @DisplayName("Evaluate return correct value for finish")
     void evaluateReturnCorrectValueForFinish () {
         final Double leftNumber = 2.2;
         final Double rightNumber = 4.0;
+        Double evaluateResult;
 
-        Assertions.assertEquals(rightNumber, finishOperatorToken.evaluate(leftNumber, rightNumber));
+        evaluateResult = finishOperatorToken.evaluate(leftNumber, rightNumber);
+
+        Assertions.assertEquals(rightNumber, evaluateResult);
     }
 }
