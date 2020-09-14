fcfffbf08ed296393b20d05cc1575819b0fd6fbe
nikita.gusakov <nikit.gusakoff@gmail.com>
2020-09-14 14:47:34 +0300

    Добавил метод инициализации и поправил замечания.

--- src/test/java/shaart/application/calculator/dto/token/operator/SumOperatorTokenTest.java
+++ src/test/java/shaart/application/calculator/dto/token/operator/SumOperatorTokenTest.java
@@ -1,6 +1,7 @@
 package shaart.application.calculator.dto.token.operator;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,7 +10,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 public class SumOperatorTokenTest {
 
-    SumOperatorToken sumOperatorToken = new SumOperatorToken();
+    SumOperatorToken sumOperatorToken;
+
+    @BeforeEach
+    void initialization() {
+        sumOperatorToken = new SumOperatorToken();
+    }
 
     @Test
     @DisplayName("Evaluate return correct value for sum")
@@ -17,7 +23,10 @@ public class SumOperatorTokenTest {
         final Double leftNumber = 2.2;
         final Double rightNumber = 4.0;
         final Double correctResult = leftNumber + rightNumber;
+        Double evaluateResult;
 
-        Assertions.assertEquals(correctResult, sumOperatorToken.evaluate(leftNumber, rightNumber));
+        evaluateResult = sumOperatorToken.evaluate(leftNumber, rightNumber);
+
+        Assertions.assertEquals(correctResult, evaluateResult);
     }
 }
